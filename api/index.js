3760c509b3ba6204dac83371b8286089c0d27eac
Sidemar Junior <sidemarschimmelpfennig@outlook.com>
2024-07-08 14:31:00 -0300

    update api

--- api/index.js
+++ api/index.js
@@ -3,13 +3,15 @@ const bodyParser = require('body-parser')
 const app = express()
 const cors = require('cors')
 const { v4: uuidv4 }= require('uuid')
-
+const jwt = require("jsonwebtoken")
 
 app.use(cors())
-
-
 app.use(bodyParser.urlencoded({extended : false}))
 app.use(bodyParser.json())
+
+const JWTSecret = "jwtSECRET"
+
+
 
 const DB = {
     games : [
@@ -48,10 +50,35 @@ const DB = {
     ]
 }
 
+function auth(req, res, next){
+    const authToken = req.headers['authorization']
+    if (authToken !== undefined){
+        const bearer = authToken.split(' ')
+        var token = bearer[1] 
+
+        jwt.verify(token, JWTSecret, (err, data)=>{
+                if(err){
+                    res.status(401);
+                    res.json({err:"Token inválido!"});
+                }else{
+
+                    req.token = token;
+                    req.loggedUser = {id: data.id,email: data.email};
+                    req.empresa = "Guia do programador";                
+                    next();
+                }
+            
+            }
+        )
+    }else{
+        res.status(401).json({err : " y "})
+    }
+
+  
+}
 
 
-
-app.get('/games' ,(req, res)=>{
+app.get('/games', auth ,(req, res)=>{
     res.statusCode = 200
     res.json(DB.games)
 })
@@ -129,33 +156,38 @@ app.put("/game/:id", (req, res)=>{
     }
 })
 
-app.post("/auth" ,(req, res )=>{
-    let { email, password } = req.body
-    let user
+app.post("/auth",(req, res) => {
+
+    var {email, password} = req.body;
+
     if(email != undefined){
-        if(password != undefined){
-            user = DB.users.find(user => user.email == email);
-            if(user != undefined ){
-                if(user.password == password){
-                    res.status(200)
-                    res.json({token : "TOKEN INVÁLIDO"})
-                }else{
-                    res.status(401) 
-                    res.json({err : "Credenciais Inválidas"})
-                }
+
+        var user = DB.users.find(u => u.email == email);
+        if(user != undefined){
+            if(user.password == password){
+                jwt.sign({id: user.id, email: user.email},JWTSecret,{expiresIn:'48h'},(err, token) => {
+                    if(err){
+                        res.status(400);
+                        res.json({err:"Falha interna"});
+                    }else{
+                        res.status(200);
+                        res.json({token: token});
+                    }
+                })
             }else{
-                res.status(404)
-                res.json({err : "Email não encontrado na base de dados"})
+                res.status(401);
+                res.json({err: "Credenciais inválidas!"});
             }
         }else{
-            res.status(400)
-            res.json({err : "Senha inválido"})
+            res.status(404);
+            res.json({err: "O E-mail enviado não existe na base de dados!"});
         }
+
     }else{
-        res.status(400)
-        res.json({err : "email inválido"})
+        res.status(400);
+        res.send({err: "O E-mail enviado é inválido"});
     }
-})
+});
 
 
 app.listen(3200, ()=>{
